3606dd753b01830dc77ff2cd3d5f56e1d2de4c1f
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2025-03-25 12:57:43 -0700

    update

--- backend/routes/password.route.js
+++ backend/routes/password.route.js
@@ -2,14 +2,13 @@ import express from "express"
 import { generateOTP, verifyOTP, createResetSession, resetPassword  } from "../controllers/password.js"
 const router = express.Router()
 import localVariable from "../middleware/protectRoute.js"
-import protectRoute from "../middleware/protectRoute.js"
 
      
 
-router.post("/generate-OTP", protectRoute, localVariable,  generateOTP)
-router.get("/verify-OTP", protectRoute, localVariable,  verifyOTP)
-router.get("/create-resetSession", protectRoute, localVariable,  createResetSession)
-router.put("/reset-password", protectRoute, localVariable,  resetPassword)
+router.post("/generate-OTP", localVariable,  generateOTP)
+router.get("/verify-OTP", localVariable,  verifyOTP)
+router.get("/create-resetSession", localVariable,  createResetSession)
+router.put("/reset-password",  localVariable,  resetPassword)
 
   
 

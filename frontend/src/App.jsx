4eda45fc04ece7ad9594f19e586842eb0c20078d
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2025-03-25 17:15:43 -0700

    finish update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,34 @@ import LogIn from "./pages/LogIn.jsx";
 import ForgetPassword from "./pages/ForgetPassword.jsx";
 import VerifyOTP from "./pages/VerifyOTP.jsx";
 import ResetPassword from "./pages/ResetPassword.jsx";
+
+
 import SignUp from "./pages/SignUp.jsx";
-import Home from "./pages/Home.jsx"; // New chat page
+import Home from "./pages/Home.jsx";
 import { Toaster } from "react-hot-toast";
+import useSocket from "./socketClient/useSocket";
 
 function App() {
   const { loggedInUser } = useSelector((state) => state.user);
 
+  const socket = useSocket();
+  
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={loggedInUser ? <Home /> : <Navigate to="/log-in" />} />
+        <Route
+          path="/"
+          element={loggedInUser ? <Home /> : <Navigate to="/log-in" />}
+        />
+       
         <Route path="/sign-up" element={<SignUp />} />
+        
         <Route path="/log-in" element={<LogIn />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="/verify-OTP" element={<VerifyOTP />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+         <Route path="/reset-password" element={<ResetPassword />} />
+
       </Routes>
       <Toaster />
     </BrowserRouter>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,29 +1,46 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import SideBar from "../component/SideBar";
 import MessageComponent from "../component/MessageComponent";
+import { useLocation, useNavigate } from "react-router-dom";
 import { selectedConversationSuccess } from "../redux/userSlice.js";
 
 function Home() {
   const { selectedConversation } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    if (!selectedConversation) {
-      navigate("/");     }
-  }, [selectedConversation, navigate]);
+    const handleBackButton = () => {
+      if (location.pathname === "/") {
+        dispatch(selectedConversationSuccess(null));
+      }
+    };
+
+    window.addEventListener("popstate", handleBackButton);
+    return () => {
+      window.removeEventListener("popstate", handleBackButton);
+    };
+  }, [location, navigate, dispatch]);
 
   return (
     <div className="max-w-[1400px] mx-auto flex h-screen rounded-lg">
-      {/* Sidebar: Always visible on md+ screens, only visible on sm when no conversation is selected */}
-      <div className={`w-full md:max-w-[400px] ${selectedConversation ? "hidden md:flex" : "flex"}`}>
+      {/* Sidebar: Always visible on large screens, conditionally visible on small screens */}
+      <div
+        className={`w-full md:max-w-[400px] ${
+          selectedConversation ? "hidden sm:flex" : "flex"
+        }`}
+      >
         <SideBar />
       </div>
 
       {/* Message Component: Hidden on small screens unless a conversation is selected */}
-      <div className={`w-screen md:flex-grow ${selectedConversation ? "flex" : "hidden md:flex"}`}>
+      <div
+        className={`w-full md:flex ${
+          selectedConversation ? "flex" : "hidden"
+        }`}
+      >
         <MessageComponent />
       </div>
     </div>
